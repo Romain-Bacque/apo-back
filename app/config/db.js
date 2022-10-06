726a0f9eb1eca6ae32c1f1e0ff253fe929ef6eb8
Erwann N <neveuxerwann@gmail.com>
2022-10-06 22:29:51 +0200

    Feat: Add User generation to genDataset.js

--- app/config/db.js
+++ app/config/db.js
@@ -1,15 +1,12 @@
 const debug = require('debug')('database');
 const { Client, Pool } = require("pg");
-const database = process.env.PGDATABASE;
 
-const client = new Pool();
-
-client.connect(err => {
-    if (err) {
-        debug('Connection error', err.stack);
-    } else {
-        debug(`Connected to database '${database}'`);
-    }
+const client = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: 5432,
 });
 
 module.exports = client;
--- app/data/genDataset.js
+++ app/data/genDataset.js
@@ -1,70 +1,65 @@
 const user = require('../models/User');
+const brewery = require('../models/Brewery');
+//const Brewery = new brewery();
 
-const User = new user({
-    name: 'John Deau',
-    email: 'john@gmail.com',
-    password: 'Admin420!',
-    role: 'brewer'
-});
-User.register();
+const json = require('../data/brasseries.json');
+const axios = require('axios');
 
-// const brewery = require('../models/Brewery');
-// const Brewery = new brewery();
+const striptags = require('striptags');
+const { faker } = require('@faker-js/faker');
 
-// const json = require('../data/brasseries.json');
-// const axios = require('axios');
+faker.locale = 'fr';
 
-// const striptags = require('striptags');
-// const { faker } = require('@faker-js/faker');
+const reqConfig = {
+  method: 'get',
+  url: 'https://api.geoapify.com/v1/geocode/search',
+  params: {
+    text: '',
+    apiKey: '90ce7c3086e14917a93e2b33fe67aeae'
+  }
+};
 
-// faker.locale = 'fr';
+async function getGeoLocation(address){
 
-// const reqConfig = {
-//   method: 'get',
-//   url: 'https://api.geoapify.com/v1/geocode/search',
-//   params: {
-//     text: '',
-//     apiKey: '90ce7c3086e14917a93e2b33fe67aeae'
-//   }
-// };
+  reqConfig.params.text = address;
+  const req = await axios(reqConfig);
+  return req.data.features;
+}
 
-// async function getGeoLocation(address){
+const filter = breweries => breweries.filter(e => e["properties"].name.length > 2 && e["properties"].address.length > 2);
 
-//   reqConfig.params.text = address;
-//   const req = await axios(reqConfig);
-//   return req.data.features;
-// }
+  const dataset=[];
+  const breweries = filter(json.features);
 
-// const filter = breweries => {
+  breweries.forEach(async (item, i) => {
 
-//   let filtered;
-//   filtered = breweries.filter(e => e["properties"].name.length > 2 && e["properties"].address.length > 2);
-//   return filtered;
-// }
+      if(i >= 500) return;
 
-//   const dataset=[];
-//   const breweries = filter(json.features);
-
-//   breweries.forEach(async (item, i) => {
-
-//       if(i >= 500) return;
-//       const address = await getGeoLocation(item["properties"].address);
+      const User = new user({
+        name: faker.name(),
+        email: faker.internet.email(),
+        password: 'Admin420!',
+        role: 'brewer'
+    });
     
-//       if(address[0]){
+    const brewer = await User.register();
+    const address = await getGeoLocation(item["properties"].address);
     
-//         const brewery = {
-//           title: striptags(item["properties"].name),
-//           phone: faker.phone.number(),
-//           desc: striptags(item["properties"].description),
-//           address: address[0]["properties"].formatted,
-//           img: faker.image.nature(640, 480, true),
-//           userId: '',
-//           geoLoc: {lat: address[0]["properties"]["lat"], lon: address[0]["properties"]["lon"]}
-//         }
+      if(address[0]){
+    
+        const brewery = {
+          title: striptags(item["properties"].name),
+          phone: faker.phone.number(),
+          desc: striptags(item["properties"].description),
+          address: address[0]["properties"].formatted,
+          img: faker.image.nature(640, 480, true),
+          userId: User.id,
+          geoLoc: {lat: address[0]["properties"]["lat"], lon: address[0]["properties"]["lon"]}
+        }
       
-//         dataset.push(brewery);
-//       }
+        dataset.push(brewery);
+      }
     
-//       console.log(dataset);
+      console.log(dataset);
 
-// });
+});
