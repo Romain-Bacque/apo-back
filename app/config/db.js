90fec570b1ee93bb303abbf3a77f526a29b61856
Romain Bacque <bacqueromain@orange.fr>
2022-10-07 14:14:34 +0200

    fix: minor bug fixes

--- app/config/db.js
+++ app/config/db.js
@@ -1,11 +1,11 @@
 const debug = require('debug')('database');
-const { Client, Pool } = require("pg");
+const { Pool } = require("pg");
 
 const client = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
+    user: 'apo',
+    host: 'localhost',
+    database: 'apo',
+    password: 'pass',
     port: 5432,
 });
 
